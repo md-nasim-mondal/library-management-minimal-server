a24fc17264a49f3fcca0a4f7bc06b303faa028cb
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-07-23 19:23:13 +0600

    change route order again

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api", router);
 
 // Default route
 app.get("/", (req: Request, res: Response) => {
@@ -17,17 +18,17 @@ app.get("/", (req: Request, res: Response) => {
     message: "Welcome to Library Management API",
   });
 });
-app.use("/api", router);
 
-// Not found route
-app.use( (req: Request, res: Response) => {
-  res.status(404).json({
-    success: false,
-    message: 'Route not found!',
-  });
-});
 
 // Global error handling middleware
 app.use(globalErrorHandler);
+
+// Not found route
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
 
 export default app;
