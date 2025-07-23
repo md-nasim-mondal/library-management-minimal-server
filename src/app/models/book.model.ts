85d766f764d75802158b057f81d03419195c4b1c
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-07-23 14:21:56 +0600

    made model for book and borrow

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,63 @@
+import { Schema, model } from 'mongoose';
+import { Genre, IBook } from '../interfaces/book.interface';
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+    },
+    author: {
+      type: String,
+      required: [true, 'Author is required'],
+    },
+    genre: {
+      type: String,
+      enum: {
+        values: Object.values(Genre),
+        message: '{VALUE} is not a valid genre',
+      },
+      required: [true, 'Genre is required'],
+    },
+    isbn: {
+      type: String,
+      required: [true, 'ISBN is required'],
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    copies: {
+      type: Number,
+      required: [true, 'Number of copies is required'],
+      min: [0, 'Copies must be a positive number'],
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Static method to check if a book has enough copies
+bookSchema.statics.hasEnoughCopies = async function (bookId: string, quantity: number) {
+  const book = await this.findById(bookId);
+  return book && book.copies >= quantity;
+};
+
+// Instance method to update availability based on copies
+bookSchema.methods.updateAvailability = function () {
+  this.available = this.copies > 0;
+  return this.save();
+};
+
+// Middleware: Pre-save hook to set availability based on copies
+bookSchema.pre('save', function (next) {
+  this.available = this.copies > 0;
+  next();
+});
+
+export const Book = model<IBook>('Book', bookSchema);
